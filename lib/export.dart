67cb163bac14b89ba5a7b34a8cea0b6d3d5a140d
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-06-08 21:18:38 +0300

    Add splash page

--- lib/export.dart
+++ lib/export.dart
@@ -1,4 +1,8 @@
 export 'package:flutter/material.dart';
 export 'package:weather_bloc/base/base_stateful_state.dart';
 export 'package:weather_bloc/base/base_view_model.dart';
+export 'package:weather_bloc/ui/pages/home/home_page.dart';
 export 'package:weather_bloc/ui/pages/home/home_view_model.dart';
+export 'package:weather_bloc/ui/pages/splash/splash_page.dart';
+export 'package:weather_bloc/ui/pages/splash/splash_view_model.dart';
+export 'package:weather_bloc/utils/app_colors.dart';
